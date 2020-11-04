4fd40a1ce2a668f4385f268a71c6873894c83e78
DCartist <pbannerman13@gmail.com>
2020-11-04 18:11:20 -0500

    adding root route  and api root route

--- index.py
+++ index.py
@@ -8,3 +8,18 @@ with open('programData.json') as programData:
 
 app = Flask(__name__)
 cors = CORS(app)
+
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+@app.route('/')
+def index():
+    print(data)
+    return render_template('index.html')
+@app.route('/api/', methods=['GET'])
+def apiIndex():
+    if request.method == 'GET':
+        return(jsonify(data))
+
+
+app.run(port=3000, debug=True)
