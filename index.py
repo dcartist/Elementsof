128542fea3151da97246b1925dc2a2de1e8de209
DCartist <pbannerman13@gmail.com>
2020-11-07 13:03:15 -0500

    adding api update by id and updated data

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import json
 
 # from programData import programmingData  
-with open('programData.json') as programData:
+with open('results.json') as programData:
     data = json.load(programData)
 
 app = Flask(__name__)
@@ -25,16 +25,33 @@ def programById(id=None):
     try:
         programInfo = list(filter(lambda x: x["id"] == int(id), data)) 
         print(programInfo[0]["name"])
+        summary = programInfo[0]["summary"].split("\n\n")
+        return render_template('program.html', id =id, program=programInfo[0], data = data, summary = summary)
+    
     except:
-        programInfo = [{'id': 99999, 'name': 'No Program Language Found', 'summary': 'Sorry the Language you are looking for is not listed. Please, look up another one and try again. Thank you'}]
-
-    return render_template('program.html', id =id, program=programInfo[0], data = data)
+        return render_template('error.html')
     
 
 @app.route('/api/', methods=['GET'])
 def apiIndex():
     if request.method == 'GET':
         return(jsonify(data))
+
+@app.route('/api/id/<id>', methods=['GET'])
+def apiFindById(id=None):
+    try:
+        if(list(filter(lambda x: x["id"] == int(id), data))):
+            idInformation = list(filter(lambda x: x["id"] == int(id), data))
+            return(jsonify(idInformation))
+        else:
+            errorMessage = [{'id': 99999, 'name': 'No Program Language Found', 'summary': 'Sorry the Language you are looking for is not listed. Please, look up another one and try again. Thank you'}]
+            print(errorMessage)
+            return(jsonify(errorMessage))
+    except:
+        errorMessage = {'id': 99999, 'name': 'No Program Language Found', 'summary': 'Sorry the Language you are looking for is not listed. Please, look up another one and try again. Thank you'}
+        return(jsonify(errorMessage))
+        
+
 
 
 app.run(port=3000, debug=True)
